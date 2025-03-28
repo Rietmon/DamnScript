9145419ed666148a592e6338ad54d03029dacb25
Ilya <www.sakhno23@gmail.com>
2025-03-28 15:37:44 +0100

    Fixes CI/CD

--- Scripts/build_releases.py
+++ Scripts/build_releases.py
@@ -3,14 +3,13 @@ import os
 import zipfile
 import glob
 import argparse
+import re
 from datetime import datetime
 
-# Configuration
-VERSION = "1.0.0"
-OUTPUT_DIR = "Releases"
+OUTPUT_DIR = "./Scripts/Releases"
 
-# Package configurations
-NET_RELEASE_SOURCE_DIR = "../_Binaries/Release/net7.0/"
+# Конфигурации пакетов
+NET_RELEASE_SOURCE_DIR = "./_Binaries/Release/net7.0/"
 NET_RELEASE_FILES = [
     "Antlr4.Runtime.dll",
     "DamnScript.dll",
@@ -18,7 +17,7 @@ NET_RELEASE_FILES = [
     "DamnScript.deps.json",
 ]
 
-NET_DEBUG_SOURCE_DIR = "../_Binaries/Debug/net7.0/"
+NET_DEBUG_SOURCE_DIR = "./_Binaries/Debug/net7.0/"
 NET_DEBUG_FILES = [
     "Antlr4.Runtime.dll",
     "DamnScript.dll",
@@ -26,7 +25,7 @@ NET_DEBUG_FILES = [
     "DamnScript.deps.json",
 ]
 
-UNITY_RELEASE_SOURCE_DIR = "../DamnScript/"
+UNITY_RELEASE_SOURCE_DIR = "./DamnScript/"
 UNITY_RELEASE_FILES = [
     "Runtimes/",
     "Parsings/",
@@ -41,7 +40,7 @@ UNITY_RELEASE_FILES = [
     "../_Binaries/Release/net7.0/Antlr4.Runtime.dll"
 ]
 
-UNITY_DEBUG_SOURCE_DIR = "../DamnScript/"
+UNITY_DEBUG_SOURCE_DIR = "./DamnScript/"
 UNITY_DEBUG_FILES = [
     "Runtimes/",
     "Parsings/",
@@ -57,7 +56,6 @@ UNITY_DEBUG_FILES = [
 ]
 
 def create_zip_package(source_dir, files_to_include, output_filename):
-    """Creates a zip package with specified files from source directory"""
     if not os.path.exists(OUTPUT_DIR):
         os.makedirs(OUTPUT_DIR)
 
@@ -98,48 +96,37 @@ def create_zip_package(source_dir, files_to_include, output_filename):
     return zip_path
 
 def create_net_release():
-    """Creates a .NET Release package"""
     print("\n=== Creating .NET Release Package ===")
-    output_filename = f"DamnScript_v{VERSION}_NET_Binaries_Release.zip"
+    output_filename = f"DamnScript_NET_Binaries_Release.zip"
     return create_zip_package(NET_RELEASE_SOURCE_DIR, NET_RELEASE_FILES, output_filename)
 
 def create_net_debug():
-    """Creates a .NET Debug package"""
     print("\n=== Creating .NET Debug Package ===")
-    output_filename = f"DamnScript_v{VERSION}_NET_Binaries_Debug.zip"
+    output_filename = f"DamnScript_NET_Binaries_Debug.zip"
     return create_zip_package(NET_DEBUG_SOURCE_DIR, NET_DEBUG_FILES, output_filename)
 
 def create_unity_release():
-    """Creates a Unity Release package"""
     print("\n=== Creating Unity Release Package ===")
-    output_filename = f"DamnScript_v{VERSION}_Unity_Source_Release.zip"
+    output_filename = f"DamnScript_Unity_Source_Release.zip"
     return create_zip_package(UNITY_RELEASE_SOURCE_DIR, UNITY_RELEASE_FILES, output_filename)
 
 def create_unity_debug():
-    """Creates a Unity Debug package"""
     print("\n=== Creating Unity Debug Package ===")
-    output_filename = f"DamnScript_v{VERSION}_Unity_Source_Debug.zip"
+    output_filename = f"DamnScript_Unity_Source_Debug.zip"
     return create_zip_package(UNITY_DEBUG_SOURCE_DIR, UNITY_DEBUG_FILES, output_filename)
 
 def main():
-    global VERSION
     parser = argparse.ArgumentParser(description='Create DamnScript packages')
     parser.add_argument('--all', action='store_true', help='Create all packages')
     parser.add_argument('--net-release', action='store_true', help='Create .NET Release package')
     parser.add_argument('--net-debug', action='store_true', help='Create .NET Debug package')
     parser.add_argument('--unity-release', action='store_true', help='Create Unity Release package')
     parser.add_argument('--unity-debug', action='store_true', help='Create Unity Debug package')
-    parser.add_argument('--version', type=str, help=f'Package version (default: {VERSION})')
 
     args = parser.parse_args()
 
-    # If no specific package type is selected, create all
     create_all = args.all or not (args.net_release or args.net_debug or
                                   args.unity_release or args.unity_debug)
-
-    # Update version if specified
-    if args.version:
-        VERSION = args.version
 
     if create_all or args.net_release:
         create_net_release()
